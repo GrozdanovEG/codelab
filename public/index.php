bc2ef1fa32a343499df43f464d8b6659378bfc38
Emil Grozdanov <e_grozdanov@abv.bg>
2023-02-15 15:26:31 +0000

    depl: added content sufficient for initial deployment

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" media="screen,print" href="assets/css/styles.css">
+    <title>Hello Backend</title>
+</head>
+<body>
+    <h1>Hello BackEnd </h1>
+
+    <div>
+        <p>Are you ready to become one of us?</p>
+        <p><a href="promo/926649/">See the promo!</a></p>
+    </div>
+</body>
+</html><?php
+
